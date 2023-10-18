2a656995ca23f3fb09ab7051f41cab59d0f5133a
diegoh27 <diegolancer27@gmail.com>
2023-10-18 17:07:30 -0400

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["./public/index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:  {
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+      }
+    },
+    colors: {
+      'primary':"#CC2D4A",
+      'secondary':"#8FA206",
+      'tertiary':"#61AEC9",
+      'white': "#ffff",
+      'gray-new': "#8d99ae",
+      'backgrey': "#FCFBFB"
+    },
+    fontFamily: { 
+      Montserrat: ["Monserrat", "sans-serif"],
+    },
+    textColor: theme => ({
+			...theme('colors'),
+			primary: '#CC2D4A',
+			secondary: '#8FA206',
+			terciary: '#CCCCCC',
+		}),
+    variants: {
+      width: ["responsive", "hover", "focus"],
+      extend: {},
+    }
+  },
+  plugins: [],
+}
+
